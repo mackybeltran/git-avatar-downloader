caf5e2540e91e91321b535844ca93337fbe5536d
macky <mackybeltran1@gmail.com>
2017-02-13 23:33:32 +0000

    set up user details for github api access

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
 var request = require('request');
+var GITHUB_USER = "mackybeltran"
+var GITHUB_TOKEN = "8a9997eb1f27395b4a83591908df938dbce044ae"
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
